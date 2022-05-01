451b0cf9a594079e7505832be0063b061d11df77
Mykhailo Chernov <m.chernov@astoundcommerce.com>
2022-05-01 02:57:15 +0200

    Text 3D

---
+++ src/script.js
@@ -0,0 +1,239 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import gsap from 'gsap'
+import * as gui from 'dat.gui'
+
+const parameters = {
+  color: '#ff0000',
+  spin: () => {
+    gsap.to(sphere.rotation, {
+      duration: 1,
+      y: sphere.rotation.y + Math.PI * 2,
+    })
+  },
+}
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const codeblock = {
+  shouldBeVisible: false,
+  isVisible: false,
+  mouseDownResize: false,
+}
+
+const GUI = new gui.GUI()
+
+GUI.add(codeblock, 'shouldBeVisible').name('Show codeblock')
+GUI.addColor(parameters, 'color').onChange(() => {
+  material.color.set(parameters.color)
+})
+GUI.add(parameters, 'spin')
+
+const textureLoader = new THREE.TextureLoader()
+const fontLoader = new FontLoader()
+
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+  const textGeometry = new TextGeometry('Mykhailo Chernov', {
+    font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 10,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 3,
+  })
+  const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+  const text = new THREE.Mesh(textGeometry, textMaterial)
+  textGeometry.center()
+  material.wireframe = true
+
+  scene.add(text)
+})
+
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 })
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+for (let i = 0; i < 300; i++) {
+  const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+
+  donut.position.x = (Math.random() - 0.5) * 10
+  donut.position.y = (Math.random() - 0.5) * 10
+  donut.position.z = (Math.random() - 0.5) * 10
+
+  donut.rotation.x = Math.random() * Math.PI
+  donut.rotation.y = Math.random() * Math.PI
+
+  const scale = Math.random()
+  donut.scale.set(scale, scale, scale)
+  scene.add(donut)
+}
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+
+scene.add(pointLight)
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('resize', () => updateSizes)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+  // const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Appear/disapear codeblock on the page
+  if (codeblock.shouldBeVisible != codeblock.isVisible) {
+    toogleCodeblock()
+  }
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
+
+/*
+ * Helpers
+ */
+
+const resizeButton = document.getElementById('resize-btn')
+const codeblockElement = document.getElementById('codeblock')
+const codeblockScreenPosition = codeblockElement.getBoundingClientRect()
+
+function updateSizes(width = window.innerWidth, height = window.innerHeight) {
+  // Update sizes
+  sizes.width = width
+  sizes.height = height
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+}
+
+function toogleCodeblock() {
+  codeblock.isVisible = !codeblock.isVisible
+
+  if (codeblock.isVisible) {
+    codeblockElement.style.marginLeft = '50%'
+    updateSizes(window.innerWidth / 2, window.innerHeight)
+    stickGuiLeft()
+  } else {
+    codeblockElement.style.marginLeft = '100%'
+    codeblockElement.style.width = '50vw'
+    updateSizes()
+    stickGuiRight()
+  }
+}
+
+function stickGuiLeft() {
+  document.getElementsByClassName('dg main a')[0].style.float = 'left'
+}
+;``
+function stickGuiRight() {
+  document.getElementsByClassName('dg main a')[0].style.float = 'right'
+}
+
+function handleMouseMove(e) {
+  const availableZone = e.clientX < window.innerWidth - 35
+  const { shouldBeVisible, mouseDownResize, isVisible } = codeblock
+
+  if (!availableZone && shouldBeVisible && mouseDownResize) {
+    codeblock.shouldBeVisible = !shouldBeVisible
+    codeblock.mouseDownResize = false
+  }
+
+  if (isVisible && availableZone && shouldBeVisible && mouseDownResize) {
+    codeblockElement.style.marginLeft = e.clientX + 'px'
+
+    if (e.clientX < codeblockScreenPosition.x) {
+      codeblockElement.style.width = window.innerWidth - e.clientX + 'px'
+    }
+
+    updateSizes(e.clientX, window.innerHeight)
+  }
+}
+
+resizeButton.addEventListener('mousedown', function () {
+  codeblock.mouseDownResize = true
+  document.addEventListener('mousemove', handleMouseMove)
+})
+
+resizeButton.addEventListener('mouseup', function () {
+  codeblock.mouseDownResize = false
+})
+
+document.addEventListener('keypress', (e) => {
+  const { shouldBeVisible } = codeblock
+
+  if (e.code === 'KeyG') {
+    codeblock.shouldBeVisible = !shouldBeVisible
+    console.log('Toggle code block')
+  }
+})
