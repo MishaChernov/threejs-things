3d17e339bbb2d6873c788144ae4776439d197181
Mykhailo Chernov <m.chernov@astoundcommerce.com>
2022-06-09 01:27:56 +0200

    Updates

--- bundle.ff004de7ac56b909.js
+++ bundle.ff004de7ac56b909.js
@@ -5,14 +5,14 @@
 /***/ 72:
 /***/ ((module) => {
 
-module.exports = "uniform vec3 uColorBottom;\nuniform vec3 uColorHigh;\nuniform float uColorOffset;\nuniform float uColorMultiplier;\n\nvarying float vElevation;\n\nvoid main() {\n  float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;\n  vec3 bottomColor = uColorBottom;\n  vec3 mixedColor = mix(bottomColor, uColorHigh, mixStrength);\n  gl_FragColor = vec4(mixedColor, 1.0);\n}";
+module.exports = "uniform vec3 uColorBottom;\nuniform vec3 uColorHigh;\nuniform float uColorOffset;\nuniform float uColorMultiplier;\n\nvarying float vElevation;\n\nvoid main() {\n  float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;\n  vec3 mixedColor = mix(uColorBottom, uColorHigh, mixStrength);\n  \n  gl_FragColor = vec4(mixedColor, 1.0);\n}";
 
 /***/ }),
 
 /***/ 320:
 /***/ ((module) => {
 
-module.exports = "uniform float uTime;\nuniform vec2 uFrequency;\nuniform vec2 uBigWavesFrequency;\nuniform float uBigWavesElevation;\nuniform float uBigWavesSpeed;\n\nvarying vec2 vUv;\nvarying float vElevation;\n\n\nvoid main() {\n    vec4 modelPosition = modelMatrix * vec4(position, 1.0);\n    // modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.001;\n    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * uBigWavesElevation;\n    elevation *= sin(modelPosition.z * uBigWavesFrequency.y) * uBigWavesElevation;\n\n    modelPosition.y += elevation;\n\n    vec4 viewPosition = viewMatrix * modelPosition;\n    vec4 projectedPosition = projectionMatrix * viewPosition;\n\n    gl_Position = projectedPosition;\n\n    vUv = uv;\n    vElevation = elevation;\n}";
+module.exports = "uniform float uTime;\nuniform vec2 uFrequency;\nuniform vec2 uBigWavesFrequency;\nuniform float uBigWavesElevation;\nuniform float uBigWavesSpeed;\nuniform float uSmallWavesElevation;\nuniform float uSmallWavesFrequency;\nuniform float uSmallWavesSpeed;\nuniform float uSmallIterations;\n\nvarying vec2 vUv;\nvarying float vElevation;\n\n// Classic Perlin 3D Noise \n// by Stefan Gustavson\n//\nvec4 permute(vec4 x)\n{\n    return mod(((x*34.0)+1.0)*x, 289.0);\n}\nvec4 taylorInvSqrt(vec4 r)\n{\n    return 1.79284291400159 - 0.85373472095314 * r;\n}\nvec3 fade(vec3 t)\n{\n    return t*t*t*(t*(t*6.0-15.0)+10.0);\n}\n\nfloat cnoise(vec3 P)\n{\n    vec3 Pi0 = floor(P); // Integer part for indexing\n    vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1\n    Pi0 = mod(Pi0, 289.0);\n    Pi1 = mod(Pi1, 289.0);\n    vec3 Pf0 = fract(P); // Fractional part for interpolation\n    vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0\n    vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);\n    vec4 iy = vec4(Pi0.yy, Pi1.yy);\n    vec4 iz0 = Pi0.zzzz;\n    vec4 iz1 = Pi1.zzzz;\n\n    vec4 ixy = permute(permute(ix) + iy);\n    vec4 ixy0 = permute(ixy + iz0);\n    vec4 ixy1 = permute(ixy + iz1);\n\n    vec4 gx0 = ixy0 / 7.0;\n    vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;\n    gx0 = fract(gx0);\n    vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);\n    vec4 sz0 = step(gz0, vec4(0.0));\n    gx0 -= sz0 * (step(0.0, gx0) - 0.5);\n    gy0 -= sz0 * (step(0.0, gy0) - 0.5);\n\n    vec4 gx1 = ixy1 / 7.0;\n    vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;\n    gx1 = fract(gx1);\n    vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);\n    vec4 sz1 = step(gz1, vec4(0.0));\n    gx1 -= sz1 * (step(0.0, gx1) - 0.5);\n    gy1 -= sz1 * (step(0.0, gy1) - 0.5);\n\n    vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);\n    vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);\n    vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);\n    vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);\n    vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);\n    vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);\n    vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);\n    vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);\n\n    vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));\n    g000 *= norm0.x;\n    g010 *= norm0.y;\n    g100 *= norm0.z;\n    g110 *= norm0.w;\n    vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));\n    g001 *= norm1.x;\n    g011 *= norm1.y;\n    g101 *= norm1.z;\n    g111 *= norm1.w;\n\n    float n000 = dot(g000, Pf0);\n    float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));\n    float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));\n    float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));\n    float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));\n    float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));\n    float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));\n    float n111 = dot(g111, Pf1);\n\n    vec3 fade_xyz = fade(Pf0);\n    vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);\n    vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);\n    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); \n    return 2.2 * n_xyz;\n}\n\n\nvoid main() {\n    vec4 modelPosition = modelMatrix * vec4(position, 1.0);\n    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *\n                      sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *\n                      uBigWavesElevation;\n\n    for(float i = 1.0; i < uSmallIterations; i++) {\n        elevation -= abs(cnoise(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);\n    }\n\n    modelPosition.y += elevation;\n\n    vec4 viewPosition = viewMatrix * modelPosition;\n    vec4 projectedPosition = projectionMatrix * viewPosition;\n\n    gl_Position = projectedPosition;\n\n    vUv = uv;\n    vElevation = elevation;\n}";
 
 /***/ })
 
@@ -53617,9 +53617,9 @@ const init = () => {
   const debugOptions = {
     bottomColor: '#186691',
     highColor: '#9bd8ff',
-    colorOffset: 0.25,
-    colorMultiplier: 2.0,
-    bigWavesSpeed: 1,
+    colorOffset: 0.08,
+    colorMultiplier: 5.0,
+    bigWavesSpeed: 0.75,
   }
 
   /**
@@ -53637,7 +53637,7 @@ const init = () => {
    * Object
    */
 
-  const shaderGeometry = new PlaneGeometry(2, 2, 128, 128)
+  const shaderGeometry = new PlaneGeometry(2, 2, 512, 512)
 
   const shaderMaterial = new ShaderMaterial({
     vertexShader: waves_vertex,
@@ -53649,6 +53649,10 @@ const init = () => {
       uBigWavesElevation: { value: 0.2 },
       uBigWavesFrequency: { value: new Vector2(4, 1.5) },
       uBigWavesSpeed: { value: debugOptions.bigWavesSpeed },
+      uSmallWavesElevation: { value: 0.15 },
+      uSmallWavesFrequency: { value: 3.0 },
+      uSmallWavesSpeed: { value: 0.2 },
+      uSmallIterations: { value: 4 },
 
       uColorBottom: { value: new Color(debugOptions.bottomColor) },
       uColorHigh: { value: new Color(debugOptions.highColor) },
@@ -53703,6 +53707,30 @@ const init = () => {
     .max(10)
     .step(0.001)
     .name('uBigWavesSpeed')
+  gui
+    .add(shaderMaterial.uniforms.uSmallWavesElevation, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uSmallWavesElevation')
+  gui
+    .add(shaderMaterial.uniforms.uSmallWavesFrequency, 'value')
+    .min(0)
+    .max(30)
+    .step(0.001)
+    .name('uSmallWavesFrequency')
+  gui
+    .add(shaderMaterial.uniforms.uSmallWavesSpeed, 'value')
+    .min(0)
+    .max(4)
+    .step(0.001)
+    .name('uSmallWavesSpeed')
+  gui
+    .add(shaderMaterial.uniforms.uSmallIterations, 'value')
+    .min(0)
+    .max(5)
+    .step(1)
+    .name('uSmallIterations')
   gui.addColor(debugOptions, 'bottomColor').onChange(() => {
     shaderMaterial.uniforms.uColorBottom.value.set(debugOptions.bottomColor)
   })
@@ -53894,4 +53922,4 @@ code.innerHTML = init.toString()
 
 /******/ })()
 ;
-//# sourceMappingURL=bundle.e632480be120d123.js.map
+//# sourceMappingURL=bundle.ff004de7ac56b909.js.map
